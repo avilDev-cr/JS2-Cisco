016412edb5b063cf302bd7ba6832e3ba52a7155e
Andres Aviles <andrezavilesrdz@gmail.com>
2025-01-25 18:48:18 -0600

    Ejemplos de objectos

---
+++ modulo1/objects.js
@@ -0,0 +1,32 @@
+//Tipos de datos primitivos
+console.log(typeof 2.5);
+console.log(typeof "hola");
+console.log(typeof true);
+
+
+let nr = 2.5;
+Nr = nr /2;
+console.log(typeof nr);
+
+
+
+//Matriz como un tipo complejo
+let a = [10, 20, "hola", true, 50];
+a[4]= a[4] * 2;
+console.log(a[0]);
+console.log(a[2]);
+console.log(a[4]);
+
+//Un objeto como un tipo difrente de matriz
+let sampleObject = {
+    Id: 207760909,
+    delay: 20,
+    name: "Andres",
+    isPresent: true,
+    delay: 50
+
+};
+sampleObject.delay = sampleObject.delay * 2;
+console.log(sampleObject.Id);
+console.log(sampleObject.name);
+console.log(sampleObject.delay);
